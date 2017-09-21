61f2713039c3ca9650197f33f4c78634dd31b3dc
mankityau <david.mankit.yau@gmail.com>
2017-09-21 04:33:13 -0700

    Completed Q3

--- src/highway.cpp
+++ src/highway.cpp
@@ -1,45 +1,50 @@
 #include <vector>
 #include <iostream>
 #include "Car.h"
-#include "Herbie.h"
 #include "Mazda3.h"
 #include "Prius.h"
 #include "Tesla3.h"
 
-static int TYPES_OF_CARS = 4;
 static double TARGET_SPEED = 23.8;
 static double TARGET_DURATION = 180; //3 minutes
 static double DT = 0.1;
 
-Car* createRandomCar();
-
 int main() {
-  std::vector<Car*> cars;
-  for(int i = 0; i < 100; i ++){
-    cars.push_back(createRandomCar());
-  }
+  Car* carTesla[100];
+  Car** carMazda = new Car*[100];
+  std::vector<Car*> carPrius(100);
 
+  for (int i=0; i<100; ++i) {
+    carTesla[i] = new Prius();   // fixed-size array
+    carMazda[i] = new Mazda3();  // dynamic array
+    carPrius[i] = new Tesla3();  // vector
+  }
   for (double timePassed = 0; timePassed < TARGET_DURATION; timePassed += DT){
-    for (Car* car: cars){
+    for (Car* car: carTesla){
+      car->accelerate(car->getState()->velocity < TARGET_SPEED);
+      car->drive(DT);
+      std::cout << *car << std::endl;
+    }
+
+    for (int i=0; i<100; ++i) {
+      carMazda[i]->accelerate(carMazda[i]->getState()->velocity < TARGET_SPEED);
+      carMazda[i]->drive(DT);
+      std::cout << *carMazda[i] << std::endl;
+    }
+
+    for (Car* car: carPrius) {
       car->accelerate(car->getState()->velocity < TARGET_SPEED);
       car->drive(DT);
       std::cout << *car << std::endl;
     }
   }
-  return 0;
-}
 
-Car* createRandomCar(){
-  switch (rand() % TYPES_OF_CARS) {
-    case 0:
-      return new Herbie();
-    case 1:
-      return new Mazda3();
-    case 2:
-      return new Prius();
-    case 3:
-      return new Tesla3();
-    default:
-      throw "Failed to generate cars";
+  for (int i=0; i<100; ++i) {
+    delete carTesla[i];  // fixed-size array
+    delete carMazda[i];  // dynamic array
+    delete carPrius[i];  // vector
   }
+
+  delete[] carMazda;
+  return 0;
 }
