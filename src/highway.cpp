be04a08f303f96c01f3f3496f674fdd495898d95
mankityau <david.mankit.yau@gmail.com>
2017-09-21 04:14:47 -0700

    Finished Q2

--- src/highway.cpp
+++ src/highway.cpp
@@ -1,10 +1,45 @@
 #include <vector>
 #include <iostream>
 #include "Car.h"
+#include "Herbie.h"
+#include "Mazda3.h"
+#include "Prius.h"
+#include "Tesla3.h"
+
+static int TYPES_OF_CARS = 4;
+static double TARGET_SPEED = 23.8;
+static double TARGET_DURATION = 180; //3 minutes
+static double DT = 0.1;
+
+Car* createRandomCar();
 
 int main() {
+  std::vector<Car*> cars;
+  for(int i = 0; i < 100; i ++){
+    cars.push_back(createRandomCar());
+  }
 
-  // Your code here
-
+  for (double timePassed = 0; timePassed < TARGET_DURATION; timePassed += DT){
+    for (Car* car: cars){
+      car->accelerate(car->getState()->velocity < TARGET_SPEED);
+      car->drive(DT);
+      std::cout << *car << std::endl;
+    }
+  }
   return 0;
+}
+
+Car* createRandomCar(){
+  switch (rand() % TYPES_OF_CARS) {
+    case 0:
+      return new Herbie();
+    case 1:
+      return new Mazda3();
+    case 2:
+      return new Prius();
+    case 3:
+      return new Tesla3();
+    default:
+      throw "Failed to generate cars";
+  }
 }
